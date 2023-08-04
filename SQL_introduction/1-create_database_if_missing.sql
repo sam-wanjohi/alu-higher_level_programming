a0d6313149632af1d06c6a101363d912227d1a79
Samuel Wanjohi <s.wanjohi@alustudent.com>
2023-08-04 08:22:42 -0700

    all

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creating a databasa in MYSQL
+-- only if does not exists.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- write a script that lists all the tables in the database in mySQL server
+SHOW TABLES;
