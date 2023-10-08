6562d04e608811a14b9de8b6b07b990263a42e9f
Samuel Wanjohi <s.wanjohi@alustudent.com>
2023-10-08 10:06:33 -0700

    yes

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2], 10);
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return (1);
+  } else {
+    return (factorial(n - 1) * n);
+  }
+}
+console.log(factorial(num));
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const string of stringArray) {
+  console.log(string);
+}
